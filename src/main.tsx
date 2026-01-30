f21d12ecabb852c1cc7f1c5c10495896a3a201c4
bpou <83222082+bpou@users.noreply.github.com>
2026-01-30 17:44:15 +0100

    navigation testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import "mapbox-gl/dist/mapbox-gl.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
